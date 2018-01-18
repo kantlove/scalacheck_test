0bcf13d0b1e96135a14c9c632b4ec3bebe17569c
minhthai <phammt@garena.com>
2018-01-18 19:26:24 +0800

    Add custom generators

--- src/test/scala/StringTest.scala
+++ src/test/scala/StringTest.scala
@@ -1,5 +1,5 @@
 import org.scalacheck.Prop.forAll
-import org.scalacheck.Properties
+import org.scalacheck.{Properties, Shrink, Gen}
 
 object StringTest extends Properties("String") {
 
@@ -14,4 +14,31 @@ object StringTest extends Properties("String") {
   property("substring") = forAll { (a: String, b: String, c: String) =>
     (a + b + c).substring(a.length, a.length + b.length) == b
   }
+
+  val personGen: Gen[Person] = {
+    for {
+      name <- Gen.alphaLowerStr
+      age <- Gen.choose(1, 100)
+    } yield Person(name, age)
+  }
+
+  /**
+    * If there is not enough test cases satisfied our condition, ScalaCheck will mark testing property as "undecided".
+    * So when is "not enough"? ScalaCheck will discard a newly generated test case if it doesn't satisfy. If there are
+    * more than 500 (be default) discarded cases, it is "not enough".
+    * (this can be verified by checking the Test.scala file in the library)
+    */
+  val teenGen: Gen[Person] = personGen suchThat Person.isTeenager
+
+  property("young person") = forAll(teenGen) { (p: Person) =>
+    Person.isTeenager(p)
+  }
 }
+
+object Person {
+  def isTeenager(p: Person): Boolean = {
+    p.age <= 18
+  }
+}
+
+case class Person(name: String, age: Int)
